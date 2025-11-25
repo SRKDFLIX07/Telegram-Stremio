a2e701089484ba99cf7674e04d048b47eb85826b
SRKDFLIX07 <rsenterprises201925@gmail.com>
2025-11-25 19:32:24 +0530

    Update __main__.py

--- Backend/__main__.py
+++ Backend/__main__.py
@@ -78,3 +78,9 @@ if __name__ == '__main__':
         loop.run_until_complete(stop_services())
         loop.stop()
         logging.shutdown()  
+        from Backend.fastapi.main import app
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
