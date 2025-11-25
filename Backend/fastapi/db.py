9d6932f6d53091ddddd8744cc5d6d1edbd75587b
SRKDFLIX07 <rsenterprises201925@gmail.com>
2025-11-25 22:02:57 +0530

    Update db.py

--- Backend/fastapi/db.py
+++ Backend/fastapi/db.py
@@ -1,10 +1,10 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URI = os.getenv("MONGO_URI")
+# Heroku config var names
+MONGO_URI = os.getenv("MONGO_URI") or os.getenv("BASE_URL")
 if not MONGO_URI:
-    raise RuntimeError("MONGO_URI not set")
+    raise RuntimeError("Mongo URI not set")
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client["yourdbname"]   # apna db name yahan likho
-
